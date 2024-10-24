4b5e1d36d3de4625aaedf04c1fbb3adcc838fd0d
gauti94 <gauti1994@gmail.com>
2024-10-24 21:32:54 +0000

    scripts.js lagað

--- scripts.js
+++ scripts.js
@@ -1,27 +1,31 @@
 function checkScreenSize1() {
   const textElement = document.getElementById("get-ready");
-  if (window.innerWidth >= 800) {
-    textElement.textContent = "Get ready to get busy";
-  } else {
-    textElement.textContent = "All the news are here";
+  if (textElement) {
+    if (window.innerWidth >= 800) {
+      textElement.textContent = "Get ready to get busy";
+    } else {
+      textElement.textContent = "All the news are here";
+    }
   }
 }
 
 function checkScreenSize2() {
   const textElement = document.getElementById("book-change");
-  if (window.innerWidth >= 800) {
-    textElement.textContent =
-      "Pumpkin pie is a sweet dessert pie with a spiced, pumpkin-based custard filling. The pumpkin is a symbol of harvest time, and pumpkin pie is often eaten during the fall and early winter.";
-  } else {
-    textElement.textContent =
-      "As you pour the first glass of your favorite Chianti or Chardonnay";
+  if (textElement) {
+    if (window.innerWidth >= 800) {
+      textElement.textContent =
+        "Pumpkin pie is a sweet dessert pie with a spiced, pumpkin-based custard filling. The pumpkin is a symbol of harvest time, and pumpkin pie is often eaten during the fall and early winter.";
+    } else {
+      textElement.textContent =
+        "As you pour the first glass of your favorite Chianti or Chardonnay";
+    }
   }
 }
 
 function checkScreenSize3() {
   const textElement = document.getElementById("want-some-action");
-  if (window.innerWidth >= 800) {
-    if (textElement) {
+  if (textElement) {
+    if (window.innerWidth >= 800) {
       textElement.textContent = "Want some action?";
     } else {
       if (textElement) {
